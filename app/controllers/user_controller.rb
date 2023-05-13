0410a86dc1ebfd6cc04a717c5bec14c7b1571065
BanRhanLu <thar19893@gmail.com>
2023-05-14 02:29:38 +0800

    modify render message in user#record

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,9 +6,9 @@ class UserController < ApplicationController
       record_params = params.permit(:id).to_h.with_indifferent_access
       user_id = record_params[:id]
       records = User.find(user_id).sleep_records.order('created_at desc')
-      render(json: { success: true, error_message: nil, sleep_records: records })
+      render(json: { success: true, error: nil, sleep_records: records })
     rescue => e
-      render(json: { error: e.message })
+      render(json: { success: false, error: e.message })
     end
   end
 
