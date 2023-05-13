229a9c28314ecd5219c85dbef87edb80608e97e0
BanRhanLu <thar19893@gmail.com>
2023-05-14 02:59:46 +0800

    add sleep and wake_up methods let user can do clock-in operations to track sleep

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,15 +2,31 @@ class UserController < ApplicationController
   def index; end
 
   def record
-    begin
-      record_params = params.permit(:id).to_h.with_indifferent_access
-      user_id = record_params[:id]
-      records = User.find(user_id).sleep_records.order('created_at desc')
-      render(json: { success: true, error: nil, sleep_records: records })
-    rescue => e
-      render(json: { success: false, error: e.message })
-    end
+    record_params = params.permit(:id).to_h.with_indifferent_access
+    user_id = record_params[:id]
+    records = User.find(user_id).sleep_records.order('created_at desc')
+    render(json: { success: true, error: nil, sleep_records: records })
+  rescue StandardError => e
+    render(json: { success: false, error: e.message })
   end
 
- 
+  def sleep
+    sleep_params = params.permit(:id).to_h.with_indifferent_access
+    user_id = sleep_params[:id]
+    User.find(user_id).sleep_records.create(clock_in:Time.zone.now)
+    render(json: { success: true, error: nil })
+  rescue StandardError => e
+    render(json: { success: false, error: e.message })
+  end
+
+  def wake_up
+    wake_up_params = params.permit(:id, :record_id).to_h.with_indifferent_access
+    record_id = wake_up_params[:record_id]
+    record = SleepRecord.find(record_id)
+    record.clock_out = Time.zone.now
+    record.save!
+    render(json: { success: true, error: nil })
+  rescue StandardError => e
+    render(json: { success: false, error: e.message })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :user, only: [:index] do
     get :record, on: :member
+    get :sleep, on: :member
+    get :wake_up, on: :member
   end
 end
