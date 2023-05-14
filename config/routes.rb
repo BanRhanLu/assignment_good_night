6b0084b49aec5eb0015a49a112f262bf02894389
BanRhanLu <thar19893@gmail.com>
2023-05-14 17:31:01 +0800

    1.create follow_ship controller,and add 2 methods   1-1 follow_ship#show show all follow_ships record about user   1-2 follow_ship#follow user could follow any other user 2.add new route about show and follow in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
     get :record, on: :member
     get :sleep, on: :member
     get :wake_up, on: :member
+    resource :follow_ship, only: [:index] do
+      get 'show', to: 'follow_ship#show', on: :member
+      get 'follow/:follow_id', to: 'follow_ship#follow', on: :member
+    end
   end
 end
