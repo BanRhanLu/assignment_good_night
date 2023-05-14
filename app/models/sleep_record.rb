8c4cc4e272c1f433ab538a03c968c75732278907
BanRhanLu <thar19893@gmail.com>
2023-05-14 20:01:23 +0800

    add calculate_duration

--- app/models/sleep_record.rb
+++ app/models/sleep_record.rb
@@ -1,3 +1,16 @@
 class SleepRecord < ApplicationRecord
+  # relations
   belongs_to :user
+  # callbacks
+  before_save :calculate_duration, if: :check_time_start_and_end?
+
+  private
+
+  def calculate_duration
+    self.duration = (clock_out - clock_in).to_i
+  end
+
+  def check_time_start_and_end?
+    clock_in.present? && clock_out.present?
+  end
 end
